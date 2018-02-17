aa15d07ca21fb092c18862b59f550feaeeb0e21f
Vlad Shved <devil-vlad@yandex.ru>
2018-02-17 12:45:02 +0300

    Finish

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -13,13 +13,15 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         private List<Point> _points;
-        public List<int> B;
-        public int[,] G;
+        public List<double> B;
+        public double[,] G;
+        public double[] a;
+        public bool button = false;
         public Form1()
         {
             InitializeComponent();
             _points = new List<Point>();
-            B = new List<int>();
+            B = new List<double>();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -29,38 +31,96 @@ namespace WindowsFormsApplication1
 
             foreach (var point in _points)
             {
-                e.Graphics.FillEllipse(new SolidBrush(Color.Red), point.X + Width / 2, point.Y + Height / 2, 5, 5);
+                e.Graphics.FillEllipse(new SolidBrush(Color.Red), point.X + Width / 2, - point.Y + Height / 2, 5, 5);
+            }
+
+            if (button)
+            {
+                for (int i = -Width/2; i < Width/2; i++)
+                {
+                    e.Graphics.DrawLine(new Pen(new SolidBrush(Color.Blue), 5), i + Width / 2, -(float)Function(i) + Height / 2, i + 1 + Width / 2, -(float)Function(i + 1) + Height / 2);
+                }
             }
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            _points.Add(new Point(e.X-Width/2, e.Y-Height/2));
+            _points.Add(new Point(e.X-Width/2, -(e.Y-Height/2)));
             Refresh();
+        }
+
+        public double Function(double x){
+            double sum = 0.0;
+            for (int i = 0; i<_points.Count; i++){
+                sum += a[i]*Math.Pow(x,i);
+            }
+            return sum;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             for (int i = 0; i < _points.Count; i++){
-                int temp = 0;
+                double temp = 0;
                 for (int j = 0; j < _points.Count; j++){
-                    temp += _points[j].Y * (int)Math.Pow(_points[j].X, i);  
+                    temp += _points[j].Y * (double)Math.Pow(_points[j].X, i);  
     
                 }
                 B.Add(temp);
 
             }
-            G = new int[_points.Count, _points.Count];
+            G = new double[_points.Count, _points.Count];
+            a = new double[_points.Count];
             for (int i = 0; i < _points.Count; i++)
             {
                 for (int j = 0; j < _points.Count; j++)
                 {
                     for (int k = 0; k < _points.Count; k++)
                     {
-                        G[i, j] += (int)Math.Pow(_points[k].X, i) * (int)Math.Pow(_points[k].X, j);
+                        G[i, j] += (double)Math.Pow(_points[k].X, i) * (double)Math.Pow(_points[k].X, j);
                     }
                 }
             }
+            for (int k = 0; k < _points.Count; k++)
+            {
+                for (int i = k+1; i < _points.Count; i++)
+                {
+                    double mult = G[i, k] / G[k, k];
+                    B[i] -= B[k] * mult;
+                    for (int j = k; j < _points.Count; j++)
+                    {
+                        G[i, j] = G[i, j] - (G[k, j] * mult);
+                    }
+                }
+            }
+            for (int i = _points.Count - 1; i >= 0; i--)
+            {
+                double sum = 0.0;
+                for (int j=i; j<_points.Count;j++){
+                 sum+=G[i,j]*a[j];   
+                }
+                a[i] = (B[i]-sum)/G[i,i];
+            }
+            string result = "";
+
+            for (int i = 0; i < _points.Count; i++)
+            {
+                for (int j = 0; j < _points.Count; j++)
+                {
+                    result += G[i, j] + " ";
+                }
+                result += "\n";
+            }
+
+            result += "\nA = ";
+
+            for (int i = 0; i < _points.Count; i++ )
+            {
+                result += a[i] + " ";
+            }
+
+            //MessageBox.Show(result);
+            button = true;
+            Refresh();
         }
     }
 }
